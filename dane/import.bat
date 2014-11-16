6ab36c19caf1fc8ebae42dd99fa571a28b6ed754
Artur Kałuża <asa@vdd.pl>
2014-11-16 23:44:51 +0100

    stanowisko dodane, trzeba dopracowac polskie znaki

--- dane/import.bat
+++ dane/import.bat
@@ -11,7 +11,7 @@ sqlldr userid=%login%/%haslo% control=data_sprzedazy.ctl log=data_sprzedazy.log
 sqlldr userid=%login%/%haslo% control=branza.ctl log=branza.log
 sqlldr userid=%login%/%haslo% control=klient.ctl log=klient.log
 sqlldr userid=%login%/%haslo% control=wojewodztwo.ctl log=wojewodztwo.log
-
+sqlldr userid=%login%/%haslo% control=stanowisko.ctl log=stanowisko.log
 goto:eof
 :error
 echo Musisz podać dane
