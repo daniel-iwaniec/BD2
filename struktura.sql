5c15d78379b8313c2b6ab7d2e04cf254124f940c
Daniel Iwaniec <daniel.iwaniec92@gmail.com>
2014-11-16 15:03:26 +0100

    poprawki do struktury

--- struktura.sql
+++ struktura.sql
@@ -4,12 +4,12 @@ DROP TABLE branza;
 DROP TABLE produkt;
 DROP TABLE jednostka_miary;
 DROP TABLE lokalizacja;
-DROP TABLE wojewodztwo;
 DROP TABLE miasto;
+DROP TABLE wojewodztwo;
 DROP TABLE data_sprzedazy;
-DROP TABLE rok;
-DROP TABLE miesiac;
 DROP TABLE dzien;
+DROP TABLE miesiac;
+DROP TABLE rok;
 DROP TABLE sprzedawca;
 DROP TABLE stanowisko;
 
@@ -42,86 +42,90 @@ CREATE TABLE sprzedawca (
   CONSTRAINT sprzedawca_stanowisko_fk FOREIGN KEY(stanowisko_id) REFERENCES stanowisko(id)
 );
 
-CREATE TABLE dzien (
+CREATE TABLE rok (
   id INTEGER NOT NULL,
-  CONSTRAINT dzien_pk PRIMARY KEY(id)
+  numer INTEGER NOT NULL,
+  czy_przestepny NUMBER(1,0) NOT NULL,
+  CONSTRAINT rok_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE miesiac (
   id INTEGER NOT NULL,
-  dzien_id INTEGER,
+  rok_id INTEGER,
   numer INTEGER NOT NULL,
   nazwa VARCHAR2(50) NOT NULL,
-  CONSTRAINT miesiac_pk PRIMARY KEY(id),
-  CONSTRAINT miesiac_dzien_fk FOREIGN KEY(dzien_id) REFERENCES dzien(id)
+  CONSTRAINT miesiac_pk PRIMARY KEY (id),
+  CONSTRAINT miesiac_rok_fk FOREIGN KEY (rok_id) REFERENCES rok (id)
 );
 
-CREATE TABLE rok (
+CREATE TABLE dzien (
   id INTEGER NOT NULL,
-  miesiac_id INTEGER,
-  numer INTEGER,
-  CONSTRAINT rok_pk PRIMARY KEY(id),
-  CONSTRAINT rok_miesiac_fk FOREIGN KEY(miesiac_id) REFERENCES miesiac(id)
+  miesiac_id INTEGER NOT NULL,
+  numer INTEGER NOT NULL,
+  nazwa VARCHAR2(50) NOT NULL,
+  czy_dzien_roboczy NUMBER(1) NOT NULL,
+  CONSTRAINT dzien_pk PRIMARY KEY (id),
+  CONSTRAINT dzien_miesiac_fk FOREIGN KEY (miesiac_id) REFERENCES miesiac (id)
 );
 
 CREATE TABLE data_sprzedazy (
   id INTEGER NOT NULL,
-  rok_id INTEGER,
-  czy_dzien_wolny INTEGER,
-  CONSTRAINT data_sprzedazy_pk PRIMARY KEY(id),
-  CONSTRAINT data_sprzedazy_rok_fk FOREIGN KEY(rok_id) REFERENCES rok(id)
+  dzien_id INTEGER NOT NULL,
+  CONSTRAINT data_sprzedazy_pk PRIMARY KEY (id),
+  CONSTRAINT data_sprzedazy_dzien_fk FOREIGN KEY (dzien_id) REFERENCES dzien (id)
 );
 
 CREATE TABLE jednostka_miary(
    id INTEGER NOT NULL,
    nazwa VARCHAR2(200) NOT NULL,
    symbol VARCHAR2(200) NOT NULL,
-   CONSTRAINT jednostka_miary_pk PRIMARY KEY(id)
+   CONSTRAINT jednostka_miary_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE produkt(
   id INTEGER NOT NULL,
-  jednostka_miary_id INTEGER,
+  jednostka_miary_id INTEGER NOT NULL,
   nazwa VARCHAR2(200) NOT NULL,
-  CONSTRAINT produkt_pk PRIMARY KEY(id),
-  CONSTRAINT produkt_jednostka_miary_fk FOREIGN KEY(jednostka_miary_id) REFERENCES jednostka_miary(id)
-);
-
-CREATE TABLE miasto (
-  id INTEGER NOT NULL,
-  nazwa VARCHAR2(200),
-  CONSTRAINT miasto_pk PRIMARY KEY(id)
+  CONSTRAINT produkt_pk PRIMARY KEY (id),
+  CONSTRAINT produkt_jednostka_miary_fk FOREIGN KEY (jednostka_miary_id) REFERENCES jednostka_miary (id)
 );
 
 CREATE TABLE wojewodztwo (
   id INTEGER NOT NULL,
-  miasto_id INTEGER,
-  nazwa VARCHAR2(200),
-  CONSTRAINT wojewodztwo_pk PRIMARY KEY(id),
-  CONSTRAINT wojewodztwo_miasto_fk FOREIGN KEY(miasto_id) REFERENCES miasto(id)
+  nazwa VARCHAR2(200) NOT NULL,
+  CONSTRAINT wojewodztwo_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE miasto (
+  id INTEGER NOT NULL,
+  wojewodztwo_id INTEGER NOT NULL,
+  nazwa VARCHAR2(200) NOT NULL,
+  CONSTRAINT miasto_pk PRIMARY KEY (id),
+  CONSTRAINT miasto_wojewodztwo_fk FOREIGN KEY (wojewodztwo_id) REFERENCES wojewodztwo (id)
 );
 
 CREATE TABLE lokalizacja (
   id INTEGER NOT NULL,
-  wojewodztwo_id INTEGER,
-  nazwa VARCHAR2(200),
-  CONSTRAINT lokalizacja_pk PRIMARY KEY(id),
-  CONSTRAINT lokalizacja_wojewodztwo_fk FOREIGN KEY(wojewodztwo_id) REFERENCES wojewodztwo(id)
+  miasto_id INTEGER NOT NULL,
+  ulica VARCHAR2(200) NOT NULL,
+  kod_pocztowy VARCHAR2(200) NOT NULL,
+  CONSTRAINT lokalizacja_pk PRIMARY KEY (id),
+  CONSTRAINT lokalizacja_miasto_fk FOREIGN KEY(miasto_id) REFERENCES miasto (id)
 );
 
 CREATE TABLE sprzedaz (
   id INTEGER NOT NULL,
-  klient_id INTEGER,
-  sprzedawca_id INTEGER,
-  data_sprzedazy_id INTEGER,
-  produkt_id INTEGER,
-  lokalizacja_id INTEGER,
-  ilosc INTEGER,
-  wartosc NUMBER(12,2),
+  klient_id INTEGER NOT NULL,
+  sprzedawca_id INTEGER NOT NULL,
+  data_sprzedazy_id INTEGER NOT NULL,
+  produkt_id INTEGER NOT NULL,
+  lokalizacja_id INTEGER NOT NULL,
+  ilosc INTEGER NOT NULL,
+  wartosc NUMBER(12,2) NOT NULL,
   CONSTRAINT sprzedaz_pk PRIMARY KEY (id),
   CONSTRAINT sprzedaz_klient_fk FOREIGN KEY (klient_id) REFERENCES klient (id),
   CONSTRAINT sprzedaz_sprzedawca_fk FOREIGN KEY (sprzedawca_id) REFERENCES sprzedawca (id),
   CONSTRAINT sprzedaz_data_sprzedazy_fk FOREIGN KEY (data_sprzedazy_id) REFERENCES data_sprzedazy (id),
-  CONSTRAINT sprzedaz_produkt_fk FOREIGN KEY (produkt_id) REFERENCES produkt(id),
-  CONSTRAINT sprzedaz_lokalizacja_fk FOREIGN KEY (lokalizacja_id) REFERENCES lokalizacja(id)
+  CONSTRAINT sprzedaz_produkt_fk FOREIGN KEY (produkt_id) REFERENCES produkt (id),
+  CONSTRAINT sprzedaz_lokalizacja_fk FOREIGN KEY (lokalizacja_id) REFERENCES lokalizacja (id)
 );
