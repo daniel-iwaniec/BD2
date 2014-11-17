0a8d05b099b273feab686b3d9ccafcd265b0e30b
Daniel Iwaniec <daniel.iwaniec92@gmail.com>
2014-11-17 01:11:28 +0100

    miasto

--- dane/import.bat
+++ dane/import.bat
@@ -11,6 +11,7 @@ sqlldr userid=%login%/%haslo% control=data_sprzedazy.ctl log=data_sprzedazy.log
 sqlldr userid=%login%/%haslo% control=branza.ctl log=branza.log
 sqlldr userid=%login%/%haslo% control=klient.ctl log=klient.log
 sqlldr userid=%login%/%haslo% control=wojewodztwo.ctl log=wojewodztwo.log
+sqlldr userid=%login%/%haslo% control=miasto.ctl log=miasto.log
 sqlldr userid=%login%/%haslo% control=stanowisko.ctl log=stanowisko.log
 sqlldr userid=%login%/%haslo% control=sprzedawca.ctl log=sprzedawca.log
 sqlldr userid=%login%/%haslo% control=jednostka_miary.ctl log=jednostka_miary.log
