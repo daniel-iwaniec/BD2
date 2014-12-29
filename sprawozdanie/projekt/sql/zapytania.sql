663730e290c934a8f3695d45d09f4e93b01f6c51
Daniel Iwaniec <daniel.iwaniec92@gmail.com>
2014-12-29 16:17:27 +0100

    wyniki zaptań w pdf'ach

--- sprawozdanie/projekt/sql/zapytania.sql
+++ sprawozdanie/projekt/sql/zapytania.sql
@@ -77,4 +77,4 @@ RANK() over (PARTITION BY s.lokalizacja_id ORDER BY s.wartosc DESC) AS "RANKING"
 DENSE_RANK() over (PARTITION BY s.lokalizacja_id ORDER BY s.wartosc DESC) AS "DENSE_RANKING",
 ROW_NUMBER() over (PARTITION BY s.lokalizacja_id ORDER BY s.wartosc DESC) AS "ROW_NUMBER"
 FROM sprzedaz s
-ORDER BY s.lokalizacja_id, "dense_ranking";
+ORDER BY s.lokalizacja_id, "DENSE_RANKING";
