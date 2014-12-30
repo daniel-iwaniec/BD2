dc30ec7a31224bc06c3ff594dade92180674c720
Daniel Iwaniec <daniel.iwaniec92@gmail.com>
2014-12-30 15:38:29 +0100

    małe zmiany

--- select1.sql
+++
@@ -1,6 +0,0 @@
-SELECT produkt_id, klient_id, count(produkt_id) FROM sprzedaz
-GROUP BY rollup(produkt_id, klient_id);
-
-SELECT produkt_id, lokalizacja_id, sum(wartosc) FROM sprzedaz
-GROUP BY cube (produkt_id, lokalizacja_id);
-
